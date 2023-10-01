083caeaee2d9ae6090f06c18e9cd37b701431bef
PAKAJl <dgolubev7890@gmail.com>
2023-10-01 21:53:23 +0300

    moved User to base.py

--- main.py
+++ main.py
@@ -3,23 +3,11 @@ from telebot import types
 import settings
 import re
 from parser_atlas import get_parser_atlas_result, Ticket
+import base
+
 
 user_dict = {}
 
-class User:
-    def __init__(self):
-        self.first_town = None
-        self.sec_town = None
-        self.date = None
-        self.day = None
-        self.mounth = None
-        self.year = None
-    
-    def date_split(self):
-        dateArr = str(self.date).split('.')
-        self.day = dateArr[0]
-        self.mounth = dateArr[1]
-        self.year = dateArr[2]
 
 def atlas_send_message(user, message):
     tickets = get_parser_atlas_result(user)
